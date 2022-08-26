1973047e5094d86aa14eef123c18d2a8ea4d67b1
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2022-08-26 12:15:01 +0200

    :sparkles: feat: Navbar component + desktop >1024px styles

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,43 @@
-import './navbar.scss';
+import { NavLink } from "react-router-dom";
+import "./navbar.scss";
+import LogoNavbar from './logo.svg';
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      Navbar component
+      <nav className="navbar">
+      <div className="nav__container">
+        <div className="nav__container__logo">
+          <NavLink exact="true" to="/">
+            <div className="logo">
+              <img src={LogoNavbar} alt="Kasa logo" />
+            </div>
+          </NavLink>
+        </div>
+        <ul className="nav__container__menu">
+          <li className="homeLink">
+            <NavLink exact="true" to="/">
+              Accueil
+            </NavLink>
+          </li>
+          <li className="profilLink">
+            <NavLink exact="true" to="/profile">
+              Profil
+            </NavLink>
+          </li>
+          <li className="settingsLink">
+            <NavLink exact="true" to="/settings">
+              Réglage
+            </NavLink>
+          </li>
+          <li className="communityLink">
+            <NavLink exact="true" to="/community">
+              Communauté
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
     </div>
   );
 };
