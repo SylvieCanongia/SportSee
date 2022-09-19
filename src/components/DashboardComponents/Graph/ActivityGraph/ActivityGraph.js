685b63b793a566d2bbac440e85cbb29a2dcb12bd
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2022-09-19 11:02:27 +0200

    :sparkles: feat: added Score graph

--- src/components/DashboardComponents/Graph/ActivityGraph/ActivityGraph.js
+++ src/components/DashboardComponents/Graph/ActivityGraph/ActivityGraph.js
@@ -1,9 +1,9 @@
-import React, { PureComponent } from 'react';
-import { BarChart, Bar, ReferenceLine, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Text, ResponsiveContainer } from 'recharts';
+import React, { PureComponent } from "react";
+import { BarChart, Bar, ReferenceLine, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Text, ResponsiveContainer } from "recharts";
 
-import { USER_ACTIVITY } from '../../../../mocks/mock-data';
-import { UserActivityModel } from './../../../../service/models/UserActivityModel'
-import './activityGraph.scss';
+import { USER_ACTIVITY } from "../../../../mocks/mock-data";
+import { UserActivityModel } from "./../../../../service/models/UserActivityModel";
+import "./activityGraph.scss";
 
 const data = [
   {
@@ -37,41 +37,40 @@ const data = [
     calories: 162,
   },
   {
-    day: '7',
+    day: "7",
     kilogram: 76,
     calories: 39,
-  }
+  },
 ];
 
 const ActivityGraph = () => {
   return (
-    <div className='activityGraph'>
-    <h3 className='activityGraphTitle'>Activité quotidienne</h3>
-    <ResponsiveContainer width="100%" height="100%">
-      <BarChart
-        // width={835}
-        // height={320}
-        outerRadius={60}
-        data={data}
-        margin={{
-          top: 15,
-          right: 5,
-          left: 25,
-          bottom: 15
-        }}
-        barSize={7}
-        barGap={8}
-        // background={{fill: "#FBFBFB"}}
-      >
-      <CartesianGrid strokeDasharray="2 2" vertical={false} />
-      <XAxis dataKey="day" stroke="hsl(228, 9%, 64%)"/>
-      <YAxis stroke="hsl(228, 9%, 64%)" orientation='right'/>
-      <Tooltip />
-      <Legend verticalAlign="top" align="right" height={45} iconType="circle" iconSize="10" wrapperStyle={{ paddingRight: "10px", fontSize:"14px" }}
-      />
-      <Bar dataKey="kilogram" name="Poids (kg)" fill="hsl(203, 9%, 17%)" radius={[50, 50, 0, 0]} width={10}/>
-      <Bar dataKey="calories" name="Calories brûlées (kCal)" radius={[50, 50, 0, 0]} fill="hsl(0, 100%, 45%)" />
-      </BarChart>
+    <div className="activityGraph">
+      <h3 className="activityGraphTitle">Activité quotidienne</h3>
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          // width={835}
+          // height={320}
+          outerRadius={60}
+          data={data}
+          margin={{
+            top: 15,
+            right: 5,
+            left: 25,
+            bottom: 15,
+          }}
+          barSize={7}
+          barGap={8}
+          // background={{fill: "#FBFBFB"}}
+        >
+          <CartesianGrid strokeDasharray="2 2" vertical={false} />
+          <XAxis dataKey="day" stroke="hsl(228, 9%, 64%)" />
+          <YAxis stroke="hsl(228, 9%, 64%)" orientation="right" />
+          <Tooltip />
+          <Legend verticalAlign="top" align="right" height={45} iconType="circle" iconSize="10" wrapperStyle={{ paddingRight: "10px", fontSize: "14px" }} />
+          <Bar dataKey="kilogram" name="Poids (kg)" fill="hsl(203, 9%, 17%)" radius={[50, 50, 0, 0]} width={10} />
+          <Bar dataKey="calories" name="Calories brûlées (kCal)" radius={[50, 50, 0, 0]} fill="hsl(0, 100%, 45%)" />
+        </BarChart>
       </ResponsiveContainer>
     </div>
   );
--- src/components/DashboardComponents/Graph/Score/Score.js
+++ src/components/DashboardComponents/Graph/Score/Score.js
@@ -1,9 +1,37 @@
+import { RadialBarChart, RadialBar, Tooltip, ResponsiveContainer } from 'recharts';
+
 import './score.scss';
+
+const data = [
+  {
+    "name": "18-24",
+    "score": 0.12,
+    "fill": "hsl(0, 100%, 50%)"
+  }
+]
 
 const Score = () => {
   return (
-    <div>
-      Score
+    <div className='score'>
+    <h3 className='scoreLabel'>Score</h3>
+    <p className="scoreBarLabel"><span>12 %</span>de votre objectif</p>
+      <ResponsiveContainer width="100%" height="100%">
+        <RadialBarChart 
+          // width={730} 
+          // height={250} 
+          innerRadius="85%" 
+          outerRadius="100%" 
+          data={data} 
+          startAngle={-180} 
+          endAngle={-180 - (0.12 * 360)}
+          
+          barSize={10}
+        >
+          <RadialBar cornerRadius={50} label={false} clockWise={true} dataKey='score' />
+          <Tooltip />
+          
+        </RadialBarChart>
+      </ResponsiveContainer>
     </div>
   );
 };
