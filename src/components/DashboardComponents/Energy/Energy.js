3a72b9c56ca9652468653700e8eafe97d1546662
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2022-09-23 12:25:35 +0200

    :recycle: refactor : get data from API and axios request in Energy component

--- src/components/DashboardComponents/Energy/Energy.js
+++ src/components/DashboardComponents/Energy/Energy.js
@@ -1,5 +1,6 @@
-import { USER_MAIN_DATA } from '../../../mocks/mock-data';
-// import {getUserMainData} from '../../../service/user-http.service';
+import React, { useEffect, useState } from 'react';
+// import { USER_MAIN_DATA } from '../../../mocks/mock-data';
+import { getUserMainData } from '../../../service/user-http.service';
 import { UserMainDataModel } from '../../../service/models/UserMainDataModel';
 
 import CaloriesIcon from './calories-icon.svg';
@@ -10,41 +11,57 @@ import LipidIcon from './fat-icon.svg';
 import './energy.scss';
 
 const Energy = ({ id }) => {
-  const userData = USER_MAIN_DATA.find((userData)=> userData.id === id);
+  // MOCKED DATA --------
+  // const userData = USER_MAIN_DATA.find((userData)=> userData.id === id);
   // console.log(userData);
-
-  const user = new UserMainDataModel(userData);
+  // const user = new UserMainDataModel(userData);
   // console.log(user);
-
-  const energyData = user.keyData;
+  // const energyData = user.keyData;
   // console.log(energyData)
+  // ------------------
+
+  // API DATA
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    getUserMainData(id)
+      .then((response) => {
+        const userData = new UserMainDataModel(response.data)
+        setData(userData.keyData);
+      });
+  }, [id]);
 
   return (
+
     <div className='energy'>
-      <div className='energyCard'>
-        <div className="icon">
-          <img src={CaloriesIcon} alt="Calories icon" />
+    { data &&
+      <React.Fragment>
+        <div className='energyCard'>
+          <div className="icon">
+            <img src={CaloriesIcon} alt="Calories icon" />
+          </div>
+          <div className="energyData"><span className='energyCount'>{data.calorieCount}kCal</span><span>Calories</span> </div>
         </div>
-        <div className="energyData"><span className='energyCount'>{energyData.calorieCount}kCal</span><span>Calories</span> </div>
-      </div>
-      <div className='energyCard'>
-        <div className="icon">
-          <img src={ProteinIcon} alt="Proteins icon" />
+        <div className='energyCard'>
+          <div className="icon">
+            <img src={ProteinIcon} alt="Proteins icon" />
+          </div>
+          <div className="energyData"><span className='energyCount'>{data.proteinCount}g</span><span>Protéines</span> </div>
         </div>
-        <div className="energyData"><span className='energyCount'>{energyData.proteinCount}g</span><span>Protéines</span> </div>
-      </div>
-      <div className='energyCard'>
-        <div className="icon">
-          <img src={SugarIcon} alt="Sugars icon" />
+        <div className='energyCard'>
+          <div className="icon">
+            <img src={SugarIcon} alt="Sugars icon" />
+          </div>
+          <div className="energyData"><span className='energyCount'>{data.carbohydrateCount}g</span><span>Glucides</span></div>
         </div>
-        <div className="energyData"><span className='energyCount'>{energyData.carbohydrateCount}g</span><span>Glucides</span></div>
-      </div>
-      <div className='energyCard'>
-        <div className="icon">
-          <img src={LipidIcon} alt="Lipids icon" />
+        <div className='energyCard'>
+          <div className="icon">
+            <img src={LipidIcon} alt="Lipids icon" />
+          </div>
+          <div className="energyData"><span className='energyCount'>{data.lipidCount}g</span><span>Lipides</span> </div>
         </div>
-        <div className="energyData"><span className='energyCount'>{energyData.lipidCount}g</span><span>Lipides</span> </div>
-      </div>
+        </React.Fragment>
+    }
     </div>
   );
 };
